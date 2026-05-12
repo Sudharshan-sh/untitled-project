dd16956745cb35d095e36e56b251e436da49b99d
Sudharshan <sudharsanam346@gmail.com>
2026-05-12 18:29:45 +0530

    fixed header file declaration and port number

--- server.c
+++ server.c
@@ -3,18 +3,19 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 #define PORT 9000;
 
 int main (){
-    int server_socket, client-so
-    struct sockchk_address;
+    int server_fd, client_socket;
+    struct sockaddr_in address;
     char buffer[512];
-    server_fd=socket(AF_INET, SOCK_STREAM);
-    sockchk_address = AF_INET;
-    sockchk_address = INADDR_ANY;
-    sockchk_address = htons(9000);
-    bind(server_fd,(struct sockchk_address*)&address, sizeof(address));
+    server_fd=socket(AF_INET, SOCK_STREAM,0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(9000);
+    bind(server_fd,(struct sockaddr*)&address, sizeof(address));
     listen(server_fd,3);
     printf("Server started worker listening on the 9000 \n");
 
